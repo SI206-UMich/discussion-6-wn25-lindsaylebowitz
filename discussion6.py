759d3f770799f33a4d3fefea5f316768d01fbbd6
Lindsay Lebowitz <lindsaylebowitz@MacBook-Pro-1319.local>
2025-02-20 11:12:19 -0500

    Completed get_month_avg method

--- discussion6.py
+++ discussion6.py
@@ -100,5 +100,12 @@ class dis7_test(unittest.TestCase):
 def main():
     unittest.main(verbosity=2)
 
+    print("----------------------------------------------------------------------")
+    flight_dict = load_csv('daily_visitors.csv')
+    print("Output of load_csv:", flight_dict, "\n")
+    print("Output of get_annual_max:", get_annual_max(flight_dict), "\n")
+    print("Output of get_month_avg:", get_month_avg(flight_dict), "\n")
+
+
 if __name__ == '__main__':
     main()
